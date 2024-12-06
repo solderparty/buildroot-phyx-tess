d44aaddb5ee80efa85a022701e4c228a247001b4
Bert Outtier <outtierbert@gmail.com>
2024-12-06 20:23:01 +0100

    improve device tree

--- board/tess/flash.sh
+++ board/tess/flash.sh
@@ -2,6 +2,6 @@
 
 ./output/host/usr/bin/sunxi-fel -v uboot output/images/u-boot-sunxi-with-spl.bin \
              write 0x42000000 output/images/zImage \
-             write 0x43000000 output/images/sun8i-t113s-mangopi-mq-r-t113.dtb \
+             write 0x43000000 output/images/sun8i-t113s-tess.dtb \
              write 0x43100000 output/images/boot.scr \
              write 0x43300000 output/images/rootfs.cpio.uboot
