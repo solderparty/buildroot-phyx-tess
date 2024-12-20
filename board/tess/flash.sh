8ed9d6d719e45e9264640fb1691cc79d342da4c6
arturo182 <arturo182@tlen.pl>
2024-12-20 06:08:56 +0100

    tess: Print progress while flashing

--- board/tess/flash.sh
+++ board/tess/flash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./output/host/usr/bin/sunxi-fel -v uboot output/images/u-boot-sunxi-with-spl.bin \
+./output/host/usr/bin/sunxi-fel --progress -v uboot output/images/u-boot-sunxi-with-spl.bin \
              write 0x42000000 output/images/zImage \
              write 0x43000000 output/images/sun8i-t113s-tess.dtb \
              write 0x43100000 output/images/boot.scr \
